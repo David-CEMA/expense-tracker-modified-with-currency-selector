a1fcbfbe8f4c1c21601f5131527c33ab6238018f
David-CEMA <65813789+David-CEMA@users.noreply.github.com>
2021-02-02 01:31:37 +0000

    Half way

---
+++ src/components/Main/Form/Form.jsx
@@ -0,0 +1,114 @@
+import React, {useState, useContext} from "react";
+import {
+  TextField,
+  Typography,
+  Grid,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
+import useStyles from "./styles";
+import {v4 as uuidv4} from "uuid";
+import {ExpenseTrackerContext} from "../../../context/context";
+import {
+  incomeCategories,
+  expenseCategories,
+} from "../../../constants/categories";
+import formatDate from "../../../utils/formatDate";
+
+const initialState = {
+  amount: "",
+  category: "",
+  type: "Income",
+  date: formatDate(new Date()),
+};
+
+const Form = () => {
+  const classes = useStyles();
+  const {addTransaction} = useContext(ExpenseTrackerContext);
+
+  const createTransaction = () => {
+    const transaction = {
+      ...formData,
+      amount: Number(formData.amount),
+      id: uuidv4(),
+    };
+    addTransaction(transaction);
+    setFormData(initialState);
+  };
+
+  const [formData, setFormData] = useState(initialState);
+  console.log(formData);
+
+  const selectedCategories =
+    formData.type === "Income" ? incomeCategories : expenseCategories;
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography align="center" variant="subtitle2" gutterBottom>
+          ...
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <InputLabel>Type</InputLabel>
+          <Select
+            value={FormData.type}
+            onChange={(e) => setFormData({...formData, type: e.target.value})}>
+            <MenuItem value="Income">Income</MenuItem>
+            <MenuItem value="Expense">Expense</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <InputLabel>Category</InputLabel>
+          <Select
+            value={formData.category}
+            onChange={(e) =>
+              setFormData({...formData, category: e.target.value})
+            }>
+            {selectedCategories.map((c) => (
+              <MenuItem key={c.type} value={c.type}>
+                {c.type}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          type="number"
+          label="Amount"
+          value={formData.amount}
+          onChange={(e) => setFormData({...formData, amount: e.target.value})}
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          type="date"
+          label="Date"
+          value={formData.date}
+          onChange={(e) =>
+            setFormData({...formData, date: formatDate(e.target.value)})
+          }
+          fullWidth
+        />
+      </Grid>
+      <Button
+        className={classes.button}
+        variant="outlined"
+        color="primary"
+        fullWidth
+        onClick={createTransaction}>
+        create
+      </Button>
+    </Grid>
+  );
+};
+
+export default Form;
